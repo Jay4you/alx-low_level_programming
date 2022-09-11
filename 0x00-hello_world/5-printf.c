9c9aedade158208d92c0e92e01d1e37fd00d2c18
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-11 02:59:53 -0700

    printf task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
-* function_name - string printer
-* a blank line
-* Description: using the main function to print with proper grammar, but the
-* outcome is a piece of art,
-* section header: 
+* main - string printer
+*
+* Description: using the main function
+* to print with proper grammar, but the outcome is a piece of art,
 * Return: return 0
 */
 
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
