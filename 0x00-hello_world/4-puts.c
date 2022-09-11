934e25f2fa8088b06ccf92435ef8a79cbd97dcf3
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-11 02:12:48 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
+/**
+ *funcion_name - puts.c
+ *Description: This program prints the string "Programming is like building a multilingual puzzle"
+ */
 #include <stdio.h>
 
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle")
-    return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
