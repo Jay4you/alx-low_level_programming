31d5a47295cd5f18b6a4c4537b9ddfeaac3a49e7
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-11 02:40:00 -0700

    added task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+/**
+* function_name - string printer
+* @parameterx: Description of parameter x
+* a blank line
+* Description: using the print function to print with proper grammar, but the outcome is a piece of art,
+* section header: 
+* Return: return 0
+*/
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+    }
