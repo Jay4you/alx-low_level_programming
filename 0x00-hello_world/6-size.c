6fba67be9be42a49c011deaced756f37d421ceb1
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-11 04:27:48 -0700

    added task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,15 +12,13 @@ int main(void)
 char c;
 int i;
 long l;
-long long ll;
 double db;
 float f;
  
- printf("A char is: %cls byte(s)\n", sizeof(c));
- printf("An int is : %cls byte(s)\n", sizeof(i));
- printf("A long is: %cls byte(s)\n" sizeof(l));
- printf("A long long is: %cls byte(s)\n", sizeof(ll));
- printf("A small is: %cls byte(s)\n" sizeof(db));
- printf("A float is: %cls byte(s)\n", sizeof(f));
+ printf("A char is: %cs byte(s)\n", sizeof(c));
+ printf("An int is : %cs byte(s)\n", sizeof(i));
+ printf("A long is: %cs byte(s)\n" sizeof(l));
+ printf("A double is: %cs byte(s)\n", sizeof(db));
+ printf("A float is: %cs byte(s)\n", sizeof(f));
        return (0);
 }
