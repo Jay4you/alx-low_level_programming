aa707047cdef6b05eeba779e071999aa139e2ab6
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-12 16:06:03 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point for code
+ *
+ * Description: print single digits not married
+ * Return: Return 0
+ */
+int main(void)
+{
+int i;
+for (i = '0'; i <= '7'; i++)
+{
+putchar(i);
+if (c != '7')
+{
+putchar(',');
+putchar(',');
+}
+}
+printf("\n");
+return (0);
+}
