ca4376b7d4d39f27c886f354a0ebdafd49373c13
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-12 12:39:22 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,10 @@
 
 int main(void)
 {	
-	char low
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		printf("\n");
-	
-	return (0);
-}
+	char i;
 
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+		putchar("\n");
+		return (0);
+}
