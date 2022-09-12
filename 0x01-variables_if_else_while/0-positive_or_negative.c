410f9405aa3cb15b23fe8797ada920446e1f0690
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-12 09:51:49 -0700

    added task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,20 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - use the main funtion to generate data
  *
  * Description: this program will assign a number to the varriable n
  * Return: return 0
  */
-
- int main(void)
-  
+int main(void)  
 {
 int n;
 srand(time(0));
-
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {      
