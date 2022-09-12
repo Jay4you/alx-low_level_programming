dfcd615c0a2140352f26db70aa6dad26bbc2e41b
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-12 16:43:53 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,13 +9,9 @@
  */
 int main(void)
 {
-char c;
-int i;
 long l;
 double db;
 float f;
-printf("Size of a char: %c byte(s)\n", sizeof(c));
-printf("Size of an int: %c byte(s)\n", sizeof(i))
 printf("Size of a long int: %c byte(s)\n", sizeof(l));
 printf("Size of a long long int: %c byte(s)\n", sizeof(db));
 printf("Size of a float: %c byte(s)\n", sizeof(f));
