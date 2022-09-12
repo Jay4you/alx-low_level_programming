8b9ac5ae885c74391a3d780ad90efc23676eb63c
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-12 12:51:08 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - lower and uppercase alphabet
+ *
+ * Return: Return 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A'; i <= 'A'; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+
+	return (0)
+}
