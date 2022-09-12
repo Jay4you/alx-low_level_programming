5051271ae51e6fa90ec97eaa9048341079c55cac
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-12 12:12:03 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main function to display in lower case
+ *
+ * Description: Write a program that prints the alphabet in lowercase
+ * Return: Return 0
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+		putchar("\n")
+	}
+	return (0);
+}
