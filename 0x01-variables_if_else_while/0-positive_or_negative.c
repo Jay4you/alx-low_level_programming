b92f0a3223843fe93522edd83dfd4157e32f7846
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-12 09:30:12 -0700

    task 0 added

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,22 +12,15 @@
 int main(void)
   
 {
-  
-  int n;
-  
-  
-  srand(time(0));
-  
-  n = rand() - RAND_MAX / 2;
-  
-  if (n > 0){
-    printf("%d, is positive\n", &n);
-      }else if(n == 0) {
-    printf("%d, is zero\n", &n);
-  } else{
-      printf("%d is negative\n", &n);
-  }
+int n;
+srand(time(0));  
+n = rand() - RAND_MAX / 2
+if (n > 0){
+printf("is positive\n");
+}else if(n == 0) {
+printf("is zero\n");
+}else{
+printf("is negative\n");
 }
   return (0);
-  
 }
