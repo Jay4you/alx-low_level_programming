9d106b7054723e799a224479106e1160142e8454
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-13 13:14:21 -0700

    task  0 and readme doc added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Description: Print putchar
+ * Return: return always
+ */
+
+char main(void)
+{
+putchar("_putchar");
+putchar('\n');
+return (0);
+}
