4fd78ca2991c79d6486579b51b0fa3a0b0fca194
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-14 06:49:15 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabet
+ *
+ * @c: int argument
+ * Return: Always 0
+ */
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+_putchar(1);
+}
+else
+return (0);
+}
