0c1ba39a8ebccfb47530d8b42668eeba8d73861a
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-14 07:27:32 -0700

    task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - prrints last digit
+ *
+ * @i: argument
+ * Return: Always 0
+ */
+int print_last_digit(int)
+{
+int ln = n % 10;
+if (ln < 0)
+ln *= -1;
+_putchar(ln + '0');
+return (ln);
+}
