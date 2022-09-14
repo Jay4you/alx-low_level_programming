fc551ba74c598a02da94c7ef855ed2e6f44cf1f4
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-14 11:23:27 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,3 +4,20 @@
  *
  * Return: Always 0
  */
+void times_table(void)
+{
+int digit, multi, result;
+for (digit = 0; digit <= 9; digit++)
+{
+_putchar('0');
+for (multi = 1; multi <= 9; multi++)
+_putchar(',');
+_putchar(' ');
+result = digit * multi;
+if (result <= 9)
+_putchar(' ');
+else
+_putchar((result % 10) + '0');
+}
+_putchar('\n');
+}
