e4534335b087fb9a14a0ba06238dcbb7fe133beb
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-16 14:47:45 -0700

    task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,8 @@
 #ifdef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-
-void positive_or_negative(int);
+void positive_or_negative(int i);
 int convert_day(int, int);
 int largest_number(int, int, int);
-void print_remaining_days(int, int, int)
-
+void print_remaining_days(int, int, int);
 #endif /* MAIN_H */
