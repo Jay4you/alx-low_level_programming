34472a38a4cefead0e04b97b4d9980241a947509
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-20 07:00:50 -0700

    task 0 final push

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,10 +4,11 @@
  * reset_to_98(int *n) : funtion
  *
  * @n: pointer arg
+ * Description: pointer to reasign value to 98
  * Return: nothing
  */
 
 void reset_to_98(int *n)
 { 
-	*n = 98;
+*n = 98;
 }
