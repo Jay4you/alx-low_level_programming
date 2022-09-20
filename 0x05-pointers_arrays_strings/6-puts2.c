f51ef37996fd6367b1e5dd86091f3569f892ede0
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-20 10:56:39 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main"
+/** 
+ * puts2 - prints char
+ *
+ * @str: str
+ * Description: prints char 
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+int i =0;
+while (*(str + i) != '\0')
+{
+if (i % 2 == 0)
+putchar(*(str + 1));
+i++;
+}
+putchar(10);
+}
