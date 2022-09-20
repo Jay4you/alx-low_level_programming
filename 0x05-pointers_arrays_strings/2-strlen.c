c0fae1f351c1892df94acd9f532044f85e7a9302
Jahswill Eke <ejahswill1@gll1@gmail.com>
2022-09-20 08:59:54 -0700

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,11 +3,12 @@
  * _strlen - function entry
  * Description: returns str length
  * @s: parameter
+ * Return: return length
  */
 int _strlen(char *s)
 {
 int i = 0;
 while (*(s + i) != '\0')
 i++;
-return(i);
+return (i);
 }
