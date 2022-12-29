6c18bf3172fcbe08adc0c79b53373ec94fe44b62
jay4you <ejahswill1@gmail.com>
2022-12-29 06:58:30 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - function entry point
+ *
+ * Description: multiplies 2 digits
+ *
+ * @a: arg
+ * @b: arg
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
