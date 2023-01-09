8767fdf3b33892ecd4a154c99e6c3a38a5a7ab07
jay4you <ejahswill1@gmail.com>
2023-01-09 04:57:09 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Description : Program that prints number of arguments
+ * @argc: argument
+ * Return: 0 Always
+ */
+int main(int argc)
+{
+	printf("%d\n", argc);
+	return (0);
+}
