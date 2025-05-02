40e5ab2fab1846ceaf248ddb2c4bc573081cd59d
Jay4you <ejahswill1@gmail.com>
2025-05-02 11:44:34 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ * Program prints string
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
